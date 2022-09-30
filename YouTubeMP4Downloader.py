c66941e4d77662901fc8253ed84e0cf2c455dddc
Andrija Draca <andrija.dracaa@gmail.com>
2022-09-30 08:06:32 +0200

    Changes: The design has been redesigned and is now similar to YouTube. You can now save the downloaded video to a folder of your choice.

--- YouTubeMP4Downloader.py
+++ YouTubeMP4Downloader.py
@@ -1,6 +1,5 @@
 import webbrowser
 from pytube import YouTube
-import tkinter.messagebox
 import ttk
 import tkinter as tk
 from tkinter import filedialog
@@ -12,23 +11,25 @@ import random
 window = tk.Tk()
 window.geometry("500x300")
 window.resizable(0, 0)
-window.title("Youtube MP4 and MP3 Downloader")
-window.configure(bg="#3f3f3f")
+window.title("SYD")
+window.configure(bg="#282828")
 window.iconbitmap('images/icon1.ico')
 
 # Label Title
-mp4_title = tk.Label(window, text='YouTube MP4 and MP3 Downloader', font="Bahnschrift", fg="#d3d3d3")
-mp4_title.place(x=116, y=10)
-mp4_title.configure(bg="#3f3f3f")
+mp4_title = tk.Label(window, text='Smart YouTube Downloader', font=("YTSans Bold", 14), fg="#FF0000")
+mp4_title.place(relx=0.5,y=10,anchor=tk.CENTER)
+mp4_title.configure(bg="#282828")
 
 # Enter URL
 link = tk.StringVar()
 
-link_label = tk.Label(window, text="Paste link here:", font="Bahnschrift", fg="#d3d3d3")
-link_label.place(x=185, y=60)
-link_label.configure(bg="#3f3f3f")
-entry = tk.Entry(window, width=70, fg="#2a2a2a", textvariable=link,bg="#fbfbfb")
-entry.place(x=32, y=90)
+link_label = tk.Label(window, text="Paste link here:", font="YTSans", fg="#FF0000")
+link_label.place(relx=0.5,y=60,anchor=tk.CENTER)
+link_label.configure(bg="#282828")
+entry = tk.Entry(window, width=70, fg="#ffffff", textvariable=link,bg="#202020",highlightthickness=0.5,borderwidth=0.5)
+entry.config(highlightbackground="#313131", highlightcolor="#3ea6ff")
+
+entry.place(relx=0.5,y=90,anchor=tk.CENTER)
 
 # Functions
 def downloader():
@@ -38,11 +39,12 @@ def downloader():
     video = url.streams.get_by_resolution(resolution.get())
     video.download(path)
 
-    confirmation = tk.Label(window, text="Download Successful!", font=("Bahnschrift", 16, "bold"), fg="orange")
-    confirmation.place(x=155, y=230)
-    confirmation.configure(bg="#3f3f3f")
+    confirmation = tk.Label(window, text="Download Successful!", font=("YTSans", 16), fg="#FF0000")
+    confirmation.place(relx=0.5,y=230,anchor=tk.CENTER)
+    confirmation.configure(bg="#282828")
     window.after(2800, confirmation.destroy)
     entry.delete(0, tk.END)
+
 def downloader_mp3():
     path = filedialog.askdirectory(title="Select Directory")
     url = YouTube(str(link.get()))
@@ -51,39 +53,39 @@ def downloader_mp3():
     base, ext = os.path.splitext(downloaded_file)
     new_file = base + ".mp3"
     os.rename(downloaded_file, new_file)
-    confirmation = tk.Label(window, text="Download Successful!", font=("Bahnschrift", 16, "bold"), fg="orange")
-    confirmation.place(x=155, y=230)
-    confirmation.configure(bg="#3f3f3f")
-    window.after(2800, confirmation.destroy)
+
+    confirmation = tk.Label(window, text="Download Successful!", font=("YTSans", 16,), fg="FF0000")
+    confirmation.place(relx=0.5,y=230,anchor=tk.CENTER)
+    confirmation.configure(bg="#282828")
+
+    window.after(3500, confirmation.destroy)
 def creator_link(url):
     webbrowser.open_new(url)
 
 # Combobox etc.
-ebg = "#3f3f3f"
+ebg = "#282828"
 fg = "#d3d3d3"
 style = ttk.Style()
-style.theme_use('alt')
-window.option_add('*TCombobox*Listbox*Background', "#bebebe")
-window.option_add('*TCombobox*Listbox*Foreground', "#2a2a2a")
-window.option_add('*TCombobox*Listbox*selectBackground', fg)
-window.option_add('*TCombobox*Listbox*selectForeground', ebg)
-style.map('TCombobox', fieldbackground=[('readonly', '#d3d3d3')])
-style.map('TCombobox', selectbackground=[('readonly', '#d3d3d3')])
-style.map('TCombobox', selectforeground=[('readonly', '#2a2a2a')])
-style.map('TCombobox', background=[('readonly', "#d3d3d3")])
-style.map('TCombobox', foreground=[('readonly', "#d3d3d3")])
-resolution_label = tk.Label(window, text="Resolution:", font="Bahnschrift", fg="#d3d3d3")
-resolution_label.place(x=100, y=198)
-resolution_label.configure(bg="#3f3f3f")
-resolution = ttk.Combobox(window, state="readonly", values=["720p", "480p  (Not Working)", "360p"], )
-resolution.place(x=190, y=200)
-creator = tk.Label(window, text="Creator: DRKTRZY", fg="#d3d3d3", cursor="hand2", font=("Bahnschrift 8 underline"))
+style.theme_use('clam')
+window.option_add('*TCombobox*Listbox*Background', "#202020")
+window.option_add('*TCombobox*Listbox*Foreground', "#ffffff")
+window.option_add('*TCombobox*Listbox*selectBackground', "#313131")
+window.option_add('*TCombobox*Listbox*selectForeground', "#ffffff")
+style.map('TCombobox', fieldbackground=[('readonly', '#202020')])
+style.map('TCombobox', selectbackground=[('readonly', '#121212')])
+style.map('TCombobox', selectforeground=[('readonly', '#ffffff')])
+style.map('TCombobox', background=[('readonly', "#313131")])
+style.map('TCombobox', foreground=[('readonly', "#ffffff")])
+style.configure("TCombobox",lightcolor="#313131",darkcolor="#313131")
+resolution = ttk.Combobox(window, state="readonly", values=["720p", "480p  (Not Working)", "360p"])
+resolution.place(relx=0.5,y=200,anchor=tk.CENTER)
+creator = tk.Label(window, text="Creator: DRKTRZY", fg="#d3d3d3", cursor="hand2", font=("YTSans 8 underline"))
 creator.place(x=400, y=280)
-creator.configure(bg="#3f3f3f")
+creator.configure(bg="#282828")
 creator.bind("<Button-1>", lambda e: creator_link("https://github.com/DRKTRZY"))
 
 # Hover Button
-def custom_button(x, y, text, bcolor, fcolor, downloader):
+def custom_button(x, y, anchor,text, bcolor, fcolor, downloader):
     def on_enter(e):
         download_button["background"] = bcolor
         download_button["foreground"] = fcolor
@@ -92,20 +94,20 @@ def custom_button(x, y, text, bcolor, fcolor, downloader):
         download_button["background"] = fcolor
         download_button["foreground"] = bcolor
 
-    download_button = tk.Button(window, text=text, font="Bahnschrift", fg=bcolor, bg=fcolor, border=0,
+    download_button = tk.Button(window, text=text, font="YTSans", fg=bcolor, bg=fcolor, border=0,
                                 activeforeground=fcolor, activebackground=bcolor, padx=2, command=downloader)
     download_button.bind("<Enter>", on_enter)
     download_button.bind("<Leave>", on_leave)
-    download_button.place(x=x, y=y)
+    download_button.place(relx=x, y=y, anchor=anchor)
 
-custom_button(185, 120, "DOWNLOAD MP4", "orange", "#3f3f3f", downloader)
-custom_button(185, 155, "DOWNLOAD MP3", "orange", "#3f3f3f", downloader_mp3)
+custom_button(0.5, 120, tk.CENTER, "DOWNLOAD MP4", "#FF0000", "#282828", downloader)
+custom_button(0.5, 155,tk.CENTER, "DOWNLOAD MP3", "#FF0000", "#282828", downloader_mp3)
 
 # Secret Sound
 pygame.mixer.init()
 
 def jukebox(event):
-    music = ["Sound/Flamewall.mp3","Sound/pvrnormal.mp3"]
+    music = ["sound/Flamewall.mp3","sound/pvrnormal.mp3"]
     random_music = random.choice(music)
     pygame.mixer.music.load(random_music)
     pygame.mixer.music.play()
---
+++ tets.py
@@ -0,0 +1,6 @@
+def method(message,message2,count):
+    while count > 0:
+        print(message + message2)
+        count -= 1
+
+method("BYAS =", " GLON", 10000000)
