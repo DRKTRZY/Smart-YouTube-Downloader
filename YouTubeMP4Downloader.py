211160fd1eccbdd5647f4f44a6e0bb6a9fda808b
Andrija Draca <andrija.dracaa@gmail.com>
2022-10-19 10:32:05 +0200

    fixes and update

--- YouTubeMP4Downloader.py
+++ YouTubeMP4Downloader.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import os, pygame, random, ttk, webbrowser, tkinter.messagebox, urllib.request, io, urllib.parse,pyautogui
+import os, pygame, random, ttk, webbrowser, tkinter.messagebox, urllib.request, io, urllib.parse
 from pytube import YouTube
 from tkinter import filedialog
 from PIL import ImageTk,Image
@@ -7,7 +7,7 @@ from PIL import ImageTk,Image
 # Window
 window = tk.Tk()
 window.geometry("595x540")
-window.resizable(0, 0)
+
 window.title("SYD")
 window.configure(bg="#282828")
 window.iconbitmap('resources/SYDApp.ico')
@@ -30,7 +30,7 @@ def custom_button(x, y, anchor,text, bcolor, fcolor, downloader):
     download_button = tk.Button(set_frame, text=text, font=("YTSans 10"), fg=bcolor, bg=fcolor, border=0,activeforeground=fcolor, activebackground=bcolor, command=downloader)
     download_button.bind("<Enter>", on_enter)
     download_button.bind("<Leave>", on_leave)
-    download_button.place(relx=x, rely=y, anchor=anchor,width=100,height=30)
+    download_button.place(relx=x, rely=y, anchor=anchor,relwidth=0.215,relheight=0.5)
 
 def search_url(event):
     clearFrame()
@@ -76,8 +76,8 @@ def search_url(event):
             length.configure(text=hour)
         length.place(relx=0.52,rely=0.5,width=212.5,height=25)
         format_label = tk.Label(set_frame, text="Format:", font=("YTSans 8"), bg="#282828", fg="white")
-        format_label.place(relx=0, rely=0.35)
-        format.place(relx=0.17, rely=0.5, anchor=tk.CENTER)
+        format_label.place(relx=0, rely=0.335,relwidth=0.1099,relheight=0.375)
+        format.place(relx=0.17, rely=0.5, anchor=tk.CENTER,relwidth=0.1099,relheight=0.375)
         custom_button(0.4, 0.5, tk.CENTER, "DOWNLOAD", "#FF0000", "#282828", downloader)
 
         return image
@@ -143,16 +143,16 @@ def creator_link(url):
 # Frames
 
 set_frame = tk.Frame(window, bg="#282828",highlightbackground="green")
-set_frame.place(relx=0.5, rely=0.8, anchor=tk.CENTER, width=425, height=65)
+set_frame.place(relx=0.5, rely=0.8, anchor=tk.CENTER, relwidth=0.715, relheight=0.1)
 
 inf_frame = tk.Frame(window, bg="#282828",highlightthickness=0.01)
-inf_frame.place(relx=0.5, rely=0.203, anchor=tk.CENTER,width=425,height=65)
+inf_frame.place(relx=0.5, rely=0.203, anchor=tk.CENTER,relwidth=0.715,relheight=0.135)
 
-frame = tk.Frame(window,bg="#333333",width=600,height=60)
-frame.place(relx=0, rely=0)
+frame = tk.Frame(window,bg="#333333")
+frame.place(relx=0, rely=0,relwidth=1,relheight=0.115)
 
 thumbnail = tk.Frame(window, bg="#282828",highlightbackground="red")
-thumbnail.place(relx=0.5, rely=0.5, anchor=tk.CENTER,width=425,height=239)
+thumbnail.place(relx=0.5, rely=0.5, anchor=tk.CENTER,relwidth=0.715,relheight=0.435)
 
 # Combobox
 style = ttk.Style()
@@ -168,7 +168,7 @@ style.map('TCombobox', background=[('readonly', "#313131")])
 style.map('TCombobox', foreground=[('readonly', "#ffffff")])
 style.configure("TCombobox", lightcolor="#313131", darkcolor="#313131")
 
-format = ttk.Combobox(set_frame, state="readonly", values=["720p", "360p", ".mp3"],width=5)
+format = ttk.Combobox(set_frame, state="readonly", values=["720p", "360p", ".mp3"])
 
 # Entry etc.
 creator = tk.Label(window, text="Creator: DRKTRZY", fg="#d3d3d3", cursor="hand2", font=("YTSans 8 underline"),bg="#282828")
@@ -179,11 +179,11 @@ entry = tk.Entry(frame, width=70,fg = 'grey', textvariable=link, bg="#202020", f
 entry.insert(0, "Enter URL")
 entry.bind("<FocusIn>", on_entry_click)
 entry.bind('<FocusOut>', on_focusout)
-entry.place(relx=0.43, rely=0.5, anchor=tk.CENTER,height=42,width=500)
+entry.place(relx=0.43, rely=0.5, anchor=tk.CENTER,relheight=0.775,relwidth=0.85)
 
 search1 = ImageTk.PhotoImage(Image.open("resources/icon.png"))
 search = tk.Button(frame,image=search1,bg="#333333",activebackground="#333333",border=0,command=lambda:search_url(search))
-search.place(relx=0.89,rely=0.5,anchor=tk.CENTER)
+search.place(relx=0.89,rely=0.5,anchor=tk.CENTER,relwidth=0.06,relheight=0.6)
 # Secret Sound
 window.bind("<Return>", (lambda event: search_url(event)))
 pygame.mixer.init()
